fedaf486a53ce995cbdccde53da69a372758c09a
MARIA VITORIA MAGALHAES HOBOLD <hobold.7994@aluno.pr.senac.br>
2023-03-06 09:06:22 -0300

    to surtando

---
+++ proximo.php
@@ -0,0 +1,35 @@
+<?php
+    $nomes = array ("Dieimes","Sandra","Ana","Matheus");
+    echo $nomes[0] . " é chato.";
+    echo ("</br>");
+    echo $nomes[1] . " é professora.";
+    echo ("</br>");
+    echo $nomes[2] . " é aluna.";
+    echo ("</br>");
+    echo $nomes[3] . " é aluno.";
+
+    echo ("</br>");
+    echo ("</br>");
+
+    $carro = array ("Fusca","Gol","Corolla","Cruze");
+    echo $carro[0];
+    echo ("</br>");
+    echo $carro[1];
+    echo ("</br>");
+    echo $carro[2];
+    echo ("</br>");
+    echo $carro[3];
+
+    echo ("</br>");
+    echo ("</br>");
+
+    $paises = array ("Brasil-Brasília","Estados Unidos-Washington","Japão-Tóquio","Itália-Roma");
+    echo $paises[0];
+    echo ("</br>");
+    echo $paises[1];
+    echo ("</br>");
+    echo $paises[2];
+    echo ("</br>");
+    echo $paises[3];
+?>
+
