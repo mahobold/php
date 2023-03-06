872b30d3608a5cec236414381615ebd36dc8e5d0
MARIA VITORIA MAGALHAES HOBOLD <hobold.7994@aluno.pr.senac.br>
2023-03-06 08:41:10 -0300

    Primeiro php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    $nome = "Aline";
+    $idade = "23";
+    $endereco = "Rua das flores, 316 - Centro";
+    $cidade = "Ivaiporã";
+    $cep = "86870-000";
+    $semestre1 = 8;
+    $semestre2 = 10;
+    $semestre3 = 9;
+    $media = ($semestre1+$semestre2+$semestre3)/3;
+
+    echo "Seu nome é:" . $nome;
+    echo ("</br>");
+    echo "Sua idade é:" . $idade;
+    echo ("</br>");
+    echo "Seu endereço é:" . $endereco;
+    echo ("</br>");
+    echo "Sua cidade é:" . $cidade;
+    echo ("</br>");
+    echo "Seu CEP é:" . $cep;
+    echo ("</br>");
+    echo "Sua nota do 1° semestre é:" . $semestre1;
+    echo ("</br>");
+    echo "Sua nota do 2° semestre é:" . $semestre2;
+    echo ("</br>");
+    echo "Sua nota do 3° semestre é:" . $semestre3;
+    echo ("</br>");
+    echo "Sua média é:" . $media;
+    
+?>
